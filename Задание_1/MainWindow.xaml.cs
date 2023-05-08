cdd155257c223f034d42363a68d9ad2041e52dbf
Kisel Vladislav <d.988.1861.t@gmail.com>
2023-05-08 22:19:00 +0300

    first commit

---
+++ Задание_1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_IKT_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = null;
+            int[,] ints = new int[8, 7];
+            Random random = new Random();
+
+            int sum = 0;
+            for (int i = 0; i < ints.GetLength(0); i++)
+            {
+                ints[i, 0] = sum;
+                sum = ints[i, 0];
+                for (int j = 1; j < ints.GetLength(1); j++)
+                {
+                    ints[i, j] = random.Next(100);
+                    sum += ints[i, j];
+                }
+            }
+
+            for (int i = 0; i < ints.GetLength(0); i++)
+            {
+                textBox.Text += string.Format(@"{0,-5}{1,-5}{2,-5}{3,-5}{4,-5}{5,-5}{6,-5}", ints[i, 0], ints[i, 1], ints[i, 2], ints[i, 3], ints[i, 4], ints[i, 5], ints[i, 6]);
+                textBox.Text += "\n";
+            }
+        }
+    }
+}
---
+++ Задание_2 и 3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Задание_2_и_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text, out double quantityGoods) || !double.TryParse(textBox2.Text, out double price) && price > 0 && quantityGoods > 0)
+            {
+                MessageBox.Show("Введите коректные данные");
+                return;
+            }
+            double result = quantityGoods * price > 1000 ? (quantityGoods * price * 0.85) :(quantityGoods * price);
+            textBlock.Text = (double.Parse(textBlock.Text) + result).ToString();
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "0";
+        }
+    }
+}
